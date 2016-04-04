0142dfa9ec7531cab6710cb1443258218794aa36
Garvey <jgarvey@F5ECC.local>
2016-04-04 10:10:19 -0700

    First Commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var config ={
+			"sites":[{"label":"All Sites","value":"ALL"},
+					{"label":"BANANAS","value":"BAN"},
+					{"label":"Child Care Links","value":"CHILD"},
+					{"label":"Children and Family Resource Center","value":"CFCR"},
+					{"label":"Davis Street","value":"DAVIS"},
+					{"label":"4Cs of Alameda County","value":"C4"}],
+			"sitetypes":[{"label":"All Sites","value":"ALL"},
+						{"label":"FRIENDS AND FAMILY","value":"FFN"},
+						{"label":"FAMILY CHILD CARE","value":"FCC"},
+						{"label":"CHILD CARE CENTERS","value":"CTR"}]
+			}
